575053d95dbb394d29212b25c08beea445e8a751
Francesco-Rianna <maradonino98@hotmail.it>
2024-03-18 18:58:01 +0100

    feat: added cycle while for push number in array without repetion

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ const buttonPlay = document.querySelector('.btn');
 // prendo elemento del dom in cui devo inserire i quadrati
 const gridContainer = document.querySelector('.ms-grid');
 // inizio un ciclo for per prendermi 100 quadrati
+// creo un array vuoto per i numeri casuali del computer
+let cpuArrayNumber = []
+console.log(cpuArrayNumber)
+
 buttonPlay.addEventListener('click', function () {
     // Rimuovi eventuali quadrati già presenti nel contenitore
     gridContainer.innerHTML = '';
@@ -14,20 +18,32 @@ buttonPlay.addEventListener('click', function () {
         const newSquare = generateSquare(i);
         gridContainer.append(newSquare);
     }
+
+    // Genera 16 numeri casuali unici per cpuArrayNumber
+    while (cpuArrayNumber.length <= 16) {
+        const randomNumber = getRndInteger(1, 100);
+        if (!cpuArrayNumber.includes(randomNumber)) {
+            cpuArrayNumber.push(randomNumber);
+        }
+    }
 });
+
+
+
+
 
 function generateSquare(number) {
     // creo l'elemento del dom
-    const newSquare = document.createElement('div')
+    const newSquare = document.createElement('div');
     // gli do una classe 
-    newSquare.classList.add('ms-square')
+    newSquare.classList.add('ms-square');
     // inserisco il numero all'interno del div
-    newSquare.innerHTML = `<span>${number} </span>`
+    newSquare.innerHTML = `<span>${number} </span>`;
 
     newSquare.addEventListener('click', function () {
 
-        newSquare.classList.toggle('ms-bg-square')
-        console.log(number)
+        newSquare.classList.toggle('ms-bg-square');
+        console.log(number);
 
 
     })
@@ -41,6 +57,6 @@ function generateSquare(number) {
 
 // funzione che genera numeri casuali trovata su w3school
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;;
 }
 
